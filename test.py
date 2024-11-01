71f233cc075d9c37031116c2a936646f2b417c97
Antonio Jose <antonioalves073d@gmail.com>
2024-11-01 10:49:15 -0300

    up

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
 print ('hello')
+
+print ("mais porque ta criando apasta nodisco local? \n tanto faz o lugar \n a duvida e na hora de criar a pasta e subir 
+       
+       
+       ja esta tudo ok agr , so usar, maisainda nao entendi direito \n ai ja nao e cmg, so sei isso, manda ovideo que vc viu?\nonlocal
+         
+        \  ")
+m alatorio, posso gravar quando puder, fazendo do ZeroDivisionError
+
+
+blz, é porque quero adicionar pastas do disco local dividindo so materiais e so atividades 
+
+se nao tiver muito ocupadpo pode ver eu tentar outra ves?,,,,,, hj anoite da,,,,,, mas ja sta funcional para subir qualqur coisa, o meu fiz com backup dublo
+
+ele sincroniza com a nuvem
+
+a primeira vezque subi o que tem la foi da forma que mostrei
